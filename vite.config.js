e25d76b249233d91fbc7f0860d08b0058c89584b
dimtha01 <richarsonmartinez400@gmail.com>
2025-02-18 14:10:01 -0400

    Prueba 4

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Puerto predeterminado para el servidor de desarrollo
-    open: true, // Abrir automáticamente el navegador al iniciar el servidor
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Alias para importar archivos desde /src
-    },
-  },
-});
+})
